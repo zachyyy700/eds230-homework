687612069c58518d22f212c0dfd3d8ea4884790f
zachyyy700 <zachyyy700@gmail.com>
2026-01-21 09:01:36 -0800

    Update functions

--- 3_almondyield/almond_yield.R
+++ 3_almondyield/almond_yield.R
@@ -1,26 +1,30 @@
 
 # Define crop function
 calculate_almond_yield <- function(data){
-
-# Minimum Feb temps
-feb_temps <- climate_df |> 
+# Yearly Minimum Feb temps
+feb_temps_yearly <- data |> 
     filter(month == 2) |> 
     group_by(year) |> 
-    summarise(feb_min_temp = min(tmin_c))
+    summarise(feb_min_temps = min(tmin_c)) |> 
+    pull(feb_min_temps)
 
-# January precip
-jan_precip <- climate_df |> 
+# Yearly January precip
+jan_precip_yearly <- data |> 
     filter(month == 1) |> 
     group_by(year) |> 
-    summarise(jan_precip = sum(precip))
+    summarise(jan_precip_sum = sum(precip)) |> 
+    pull(jan_precip_sum)
 
-yields <- -0.015 * feb_temps$feb_min_temp - 0.0046 * feb_temps$feb_min_temp^2 - 0.07 * jan_precip$jan_precip + 0.0043 * jan_precip$jan_precip^2 + 0.28
+yields <- -0.015 * feb_temps_yearly - 0.0046 * feb_temps_yearly^2 - 0.07 * jan_precip_yearly + 0.0043 * jan_precip_yearly^2 + 0.28
   
 min_yield <- min(yields)
 max_yield <- max(yields)
 mean_yield <- mean(yields)
   
-output_df <- data.frame(crop = "almond", min_yield = round(min_yield, 3), max_yield = round(max_yield, 3), mean_yield = round(mean_yield, 3))
+output_df <- data.frame(crop = "almond", 
+    min_yield = round(min_yield, 3),
+    mean_yield = round(mean_yield, 3), 
+    max_yield = round(max_yield, 3))
 
 kableExtra::kable(output_df)
 } 
--- 3_almondyield/crop_yield.R
+++ 3_almondyield/crop_yield.R
@@ -3,14 +3,14 @@
 calculate_crop_yield <- function(data, crop = "almond"){
 if (crop == "almond"){
 # Minimum Feb temps
-feb_temps <- climate_df |> 
+feb_temps <- data |> 
     filter(month == 2) |> 
     group_by(year) |> 
     summarise(feb_min_temp = min(tmin_c)) |> 
     pull(feb_min_temp)
 
 # January precip
-jan_precip <- climate_df |> 
+jan_precip <- data |> 
     filter(month == 1) |> 
     group_by(year) |> 
     summarise(jan_precip = sum(precip)) |> 
@@ -20,19 +20,19 @@ yields <- -0.015 * feb_temps - 0.0046 * feb_temps^2 - 0.07 * jan_precip + 0.0043
 
 } else if (crop == "wine grapes"){
 # Minimum April temps
-april_temps <- climate_df |> 
+april_temps <- data |> 
     filter(month == 4) |> 
     group_by(year) |> 
     summarise(apr_min_temp = min(tmin_c)) |> 
     pull(apr_min_temp)
 # June precip
-june_precip <- climate_df |> 
+june_precip <- data |> 
     filter(month == 6) |> 
     group_by(year) |> 
     summarise(june_precip = sum(precip)) |> 
     pull(june_precip)
 # September precip
-sep_precip <- climate_df |> 
+sep_precip <- data |> 
     filter(month == 9) |> 
     group_by(year) |> 
     summarise(sep_precip = sum(precip)) |> 
